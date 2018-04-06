476e34a8176acd1cfbfed4298cbf7c4f29c5ab8c
Daniel Castellano <daniel.castellanom@gmail.com>
2018-04-06 17:01:49 -0600

    Missing changes.

--- Digital_Liberty/wwwroot/Main/components/Beneficiaries/Detail.js
+++ Digital_Liberty/wwwroot/Main/components/Beneficiaries/Detail.js
@@ -4,7 +4,7 @@ export default class Detail extends React.Component {
     render() {
         return (
             <div className="col-md-2 col-sm-4 col-xs-12 tile_stats_count">
-                <h1>USUARIO, {this.props.document}</h1>
+                <h1>Detalles, {this.props.document}</h1>
             </div>
         );
     }
